444ebd00ebe4f48e67c04dea4080639016576830
wetor <wetor.top@gmail.com>
2022-06-26 20:31:13 +0800

    修复help

--- cmd/root.go
+++ cmd/root.go
@@ -18,7 +18,9 @@ var rootCmd = &cobra.Command{
 	Use:     "LuckSystem",
 	Version: "1.0.0",
 	Short:   "LucaSystem引擎工具集",
-	Long:    `LucaSystem引擎工具集`,
+	Long: `LucaSystem引擎工具集
+https://github.com/wetor/LuckSystem
+wetor(wetorx@qq.com)`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
@@ -27,17 +29,18 @@ var rootCmd = &cobra.Command{
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
+
 	if Log {
 		flag.Set("alsologtostderr", "true")
 		flag.Set("log_dir", LogDir)
 		flag.Set("v", strconv.Itoa(LogLevel))
 		flag.Parse()
 	}
-
 	err := rootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
 	}
+
 }
 
 var (
@@ -47,15 +50,13 @@ var (
 )
 
 func init() {
+	if os.Args[1] == "-h" || os.Args[1] == "--help" {
+		_ = rootCmd.Help()
+		os.Exit(1)
+	}
+
 	restruct.EnableExprBeta()
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
 
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.lucksystem.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
 	rootCmd.Flags().BoolVar(&Log, "log", true, "启用日志")
 	rootCmd.Flags().IntVar(&LogLevel, "log_level", 10, "输出日志等级")
 	rootCmd.Flags().StringVar(&LogDir, "log_dir", "log", "保存日志路径")
