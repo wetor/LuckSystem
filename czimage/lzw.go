f8c9c72556168c8300036fe7cd24eb9fe15bdaec
wetor <wetorx@qq.com>
2023-12-02 20:20:27 +0800

    v2.1.1 优化代码逻辑，变量名和流程修改

--- czimage/lzw.go
+++ czimage/lzw.go
@@ -101,8 +101,8 @@ func decompressLZW(compressed []uint16, size int) []byte {
 //
 //	CZ2的LZW解压算法，暂时汇编实现
 func DecompressLZWByAsm(data []byte, size int) []byte {
-	var rax, resultIndex, rcx, rdx, dataIndex, rdi, resultSize int
-	var r8d, r9d, r10d, r11d, dataSize, r14d, dictIndex int
+	var maskBit, resultIndex, dataIndex, resultSize int
+	var dataSize, dictIndex int
 	resultSize = size // 解压长度
 	resultIndex = 0   // 解压指针
 	result := make([]byte, resultSize)
@@ -114,163 +114,113 @@ func DecompressLZWByAsm(data []byte, size int) []byte {
 	posDict := map[int]int{}
 
 	for {
-		rcx = rdi
 		posDict[dictIndex] = resultIndex
 		dictIndex++
-		rdx = int(data[dataIndex])
-		rax = 1
-		rdi++
-		rax <<= rcx & 0xFF // shl eax,cl
-		rax &= rdx
-
-		if rdi >= 8 {
-			rdx = int(data[dataIndex+1]) // movzx edx,byte ptr [rsi+01]
-			rdi = 0                      // xor edi,edi
-			dataIndex++                  // inc rsi
+		code := int(data[dataIndex])
+		flag := code & (1 << maskBit)
+		maskBit++
+		if maskBit >= 8 {
+			dataIndex++
+			code = int(data[dataIndex])
+			maskBit = 0
 		}
-		r10d = 8
-		r11d = rdx & 0xFF // movzx r11d,dl
-		rcx = rdi & 0xFF  // movzx ecx,dil
-		r10d -= rdi
-		r11d >>= rcx & 0xFF
+		dataIndex++
+		code >>= maskBit
+		codeHigh := int(data[dataIndex]) << (8 - maskBit)
 
-		dataIndex++ // inc rsi
-		r9d = rdi
-		if rax == 0 {
-			r8d = int(data[dataIndex])
-			rcx = r10d
-			r8d <<= rcx & 0xFF // shl r8d,cl
-			rdi += 0x0F
-			r8d &= 0x7FFF
-			r8d |= r11d
-			if rdi > 0x10 {
-				rdx = int(data[dataIndex+1]) // movzx edx,byte ptr [rsi+01]
-				dataIndex++                  // inc rsi
-				rcx = 0x10                   // mov ecx,00000010
-				rcx -= r9d                   // sub ecx,r9d
-				rdx <<= rcx & 0xFF           // shl edx,cl
-				rdx &= 0x7FFF                // and edx,00007FFF
-				r8d |= rdx                   // or r8d,edx
-				rdi &= 7                     // and edi,07
-			} else if rdi == 0x10 {
+		if flag == 0 {
+			code |= codeHigh & 0x7FFF
+			if maskBit > 1 {
 				dataIndex++
-				rdi &= 7
-			} else if rdi >= 0x8 { // cmp edi,08
-				rdi &= 7
+				code |= int(data[dataIndex]) << (16 - maskBit) & 0x7FFF
+			} else if maskBit == 1 {
+				dataIndex++
 			}
+			maskBit += 15
 		} else {
-			rdx = int(data[dataIndex])   // movzx edx,byte ptr [rsi]
-			rax = rdi + 0x12             // lea eax,[rdi+12]
-			r8d = int(data[dataIndex+1]) // movzx r8d,byte ptr [rsi+01]
-			dataIndex++                  // inc rsi
-			rcx = 0x10                   // mov ecx,00000010
-			rdi = rax                    // mov edi,eax
-			rcx -= r9d                   // sub ecx,r9d
-			r8d <<= rcx & 0xFF           // shl r8d,cl
-			rcx = r10d                   // mov ecx,r10d
-			r8d &= 0x3FFFF               // and r8d,0003FFFF
-			rdx <<= rcx & 0xFF           // shl edx,cl
-			r8d |= rdx                   // or r8d,edx
-			r8d |= r11d                  // or r8d,r11d
-
-			flag := false
-			if rax > 0x18 { // cmp eax,18
-				rdx = int(data[dataIndex+1]) // movzx edx,byte ptr [rsi+01]
-				dataIndex++                  // inc rsi
-				rcx = 0x18                   // mov ecx,00000018
-				rcx -= r9d                   // sub ecx,r9d`
-				rdx <<= rcx & 0xFF           // shl edx,cl
-				rdx &= 0x3FFFF               // and edx,0003FFFF
-				r8d |= rdx                   // or r8d,edx
-			} else if rax == 0x18 {
+			dataIndex++
+			code |= int(data[dataIndex]) << (16 - maskBit) & 0x3FFFF
+			code |= codeHigh
+			if maskBit > 6 {
+				dataIndex++
+				code |= int(data[dataIndex]) << (24 - maskBit) & 0x3FFFF // or r8d,edx
+			} else if maskBit == 6 {
 				dataIndex++ // inc rsi
-			} else if rax < 0x8 { // cmp eax,08
-				flag = true
 			}
-			if !flag {
-				rax = rdi                         // mov eax,edi
-				rax = int(int32(rax) &^ int32(7)) // and eax,-08
-				rdi -= rax                        // sub edi,eax
-			}
+			maskBit += 18
 		}
+		maskBit &= 7
+
 		if dataIndex > dataSize {
 			break
 		}
-		if r8d < 0x100 { // cmp r8d,0x100
-			result[resultIndex] = byte(r8d) // mov [rbx],r8l
-			resultIndex++                   // inc rbx
-			continue
-		}
-		rax = r8d // movsxd rax,r8d
-
-		r8d = posDict[rax-256] // mov r8,[r13+rax*8-00000800]
-		rdx = posDict[rax-257] // mov rdx,[r13+rax*8-00000808]
-		rcx = r8d
-		rcx -= rdx
-		rcx++
-
-		rax = rcx
-		rax += resultIndex // add rax,rbx
-		if rax >= resultSize {
-			rcx = resultSize   // mov ecx,ebp
-			rcx -= resultIndex // sub ecx,ebx
-			r8d = rcx          // movsxd  r8,ecx
-			r8d += rdx         // add r8,rdx
-		}
-		if rcx&1 != 0 {
-			result[resultIndex] = result[rdx]
-			rdx++
+		if code < 0x100 { // cmp r8d,0x100
+			result[resultIndex] = byte(code)
 			resultIndex++
-		}
-		if rcx&2 != 0 {
-			for i := 0; i < 2; i++ {
-				result[resultIndex+i] = result[rdx+i]
-			}
-			resultIndex += 2
-			rdx += 2
-		}
-		if rcx&4 != 0 {
-			for i := 0; i < 4; i++ {
-				result[resultIndex+i] = result[rdx+i]
-			}
-			resultIndex += 4
-			rdx += 4
-		}
-		if rcx&8 != 0 {
-			for i := 0; i < 8; i++ {
-				result[resultIndex+i] = result[rdx+i]
-			}
-			resultIndex += 8
-			rdx += 8
+		} else {
+			dictValueEnd := posDict[code-256]   // mov r8,[r13+rax*8-00000800]
+			dictValueIndex := posDict[code-257] // mov rdx,[r13+rax*8-00000808]
+			dictValueSize := dictValueEnd - dictValueIndex + 1
+
+			resultIndex = writeResult(result, resultIndex, resultSize,
+				dictValueIndex, dictValueSize, dictValueEnd)
 		}
 
-		if rdx >= r8d {
-			continue
-		}
-		r14d = rcx
-		r14d = int(int64(r14d) &^ int64(0xF)) // and r14,-10
-		rax = r14d + rdx
-		if resultIndex != rax {
-			r8d = r14d
-			rcx = resultIndex
-			for i := 0; i < r14d; i++ {
-				result[resultIndex+i] = result[rdx+i]
-			}
-			// ret
-			resultIndex += r14d
-			continue
-		}
-		//nop dword ptr [rax+00]
-		for rdx < r8d {
-			for i := 0; i < 0x10; i++ {
-				result[resultIndex+i] = result[rdx+i]
-			}
-			resultIndex += 0x10
-			rdx += 0x10
-			//cmp rdx,r8
-			//jb LOOPERS.exe+F0350
-		}
-		continue //jmp LOOPERS.exe+F0150
 	}
 	return result
+}
+
+func writeResult(result []byte, index, resultSize int,
+	dictValueIndex, dictValueSize, dictValueEnd int) (resultIndex int) {
+	resultIndex = index
+	if dictValueSize+resultIndex >= resultSize {
+		dictValueSize = resultSize - resultIndex
+		dictValueEnd = dictValueSize + dictValueIndex
+	}
+	if dictValueSize&1 != 0 {
+		result[resultIndex] = result[dictValueIndex]
+		dictValueIndex++
+		resultIndex++
+	}
+	if dictValueSize&2 != 0 {
+		for i := 0; i < 2; i++ {
+			result[resultIndex+i] = result[dictValueIndex+i]
+		}
+		resultIndex += 2
+		dictValueIndex += 2
+	}
+	if dictValueSize&4 != 0 {
+		for i := 0; i < 4; i++ {
+			result[resultIndex+i] = result[dictValueIndex+i]
+		}
+		resultIndex += 4
+		dictValueIndex += 4
+	}
+	if dictValueSize&8 != 0 {
+		for i := 0; i < 8; i++ {
+			result[resultIndex+i] = result[dictValueIndex+i]
+		}
+		resultIndex += 8
+		dictValueIndex += 8
+	}
+
+	if dictValueIndex < dictValueEnd {
+		copySize := int(int64(dictValueSize) &^ int64(0xF)) // and r14,-10
+		if resultIndex != copySize+dictValueIndex {
+			dictValueSize = resultIndex
+			for i := 0; i < copySize; i++ {
+				result[resultIndex+i] = result[dictValueIndex+i]
+			}
+			resultIndex += copySize
+		} else {
+			for dictValueIndex < dictValueEnd {
+				for i := 0; i < 0x10; i++ {
+					result[resultIndex+i] = result[dictValueIndex+i]
+				}
+				resultIndex += 0x10
+				dictValueIndex += 0x10
+			}
+		}
+	}
+	return resultIndex
 }
