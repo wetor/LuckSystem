e0435764704f321d75270f6ed0727d3dab665f42
danil <308240@gmail.com>
2025-06-28 14:34:16 +0300

    support for Butterfly's Poison BC Steam

---
+++ data/base/butterflyspoisonbc.py
@@ -0,0 +1,36 @@
+import core
+
+def IFN():
+    # IFN (int, expr_str, {jump})
+    core.read_uint16(True)
+    core.read_str(core.expr)
+    core.read_jump()
+    core.end()
+
+def IFY():
+    # IFY (int, expr_str, {jump})
+    core.read_uint16(True)
+    core.read_str(core.expr)
+    core.read_jump()
+    core.end()
+
+def GOTO():
+    # GOTO ({jump})
+    core.read_jump()
+    core.end()
+
+def JUMP():
+    # JUMP (int, file_str, {jump})
+    core.read_uint16(True)
+    file = core.read_str(core.expr)
+    if core.can_read():
+        core.read_jump(file)
+    core.end()
+
+def ONGOTO():
+    # ONGOTO (expr, [<const_num>, {jump}, <const_num>, {jump}, ...])
+    core.read_len_str(core.Charset_UTF8)
+    while core.can_read():
+        core.read_uint16(False)
+        core.read_jump()
+    core.end()
